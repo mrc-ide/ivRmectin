7b423673ad9d96ba1b36ed6acaab11ba19aadd34
CharlieWhittaker <cwhittaker1000@gmail.com>
2020-12-23 10:04:02 +0000

    formals(gen) no longer outputting model inputs, remove line with that call

--- R/generator_utils.R
+++ R/generator_utils.R
@@ -41,7 +41,7 @@ create_r_model <- function(odin_model_path = system.file("extdata/odin_model.R",
 
   # create odin generator
   gen <- odin::odin(odin_model_path, verbose=FALSE)
-  state <- state[names(state) %in% names(formals(gen))]
+  state <- state#[names(state) %in% names(formals(gen))]
 
   # return mod
   return(list("state"=state,"generator"=gen,"mpl"=mpl))
