eba71af3b4a840f7d7ab07340a2ed18be88c4de4
nilani-chandradeva1 <nilani.chandradeva15@imperial.ac.uk>
2023-08-09 15:12:11 +0100

    now getting warning messages on NA

--- analysis/llin_ivm_mu.R
+++ analysis/llin_ivm_mu.R
@@ -271,6 +271,31 @@ wh3a <- ivRmectin:::create_r_model(odin_model_path = "inst/extdata/odin_model_en
 res3a <- runfun(wh3a)
 df_3a <- as.data.frame(res3a)
 
+#untreated nets and ivm
+wh5 <- ivRmectin:::create_r_model(odin_model_path = "inst/extdata/odin_model_endectocide.R",
+                                  #num_int = 1,
+                                  num_int = 2,
+                                  itn_cov = 0.6,
+                                  ITN_IRS_on = 100,
+                                  #het_brackets = 5,
+                                  #age = init_age,
+                                  init_EIR = 100,
+                                  #country = "Senegal", # Country setting to be run - see admin_units_seasonal.rds in inst/extdata for more info
+                                  #admin2 = "Fatick",
+                                  ttt = ivm_parms1$ttt,
+                                  eff_len = ivm_parms1$eff_len,
+                                  haz = ivm_parms1$haz,
+                                  ivm_cov_par = ivm_parms1$ivm_cov_par,
+                                  ivm_min_age = ivm_parms1$ivm_min_age,
+                                  ivm_max_age = ivm_parms1$ivm_max_age,
+                                  IVRM_start = ivm_parms1$IVRM_start,
+                                  r_ITN0 = 0.409,
+                                  d_ITN0 = 0.059)
+
+#run Hannah's model
+res5 <- runfun(wh5)
+df_5 <- as.data.frame(res5)
+
 
 #then put into the model with constant ivm uptake, and we will sub in the mu_H value that we just estimated
 wh4 <- ivRmectin:::create_r_model(odin_model_path = "inst/extdata/odin_endec_mu_h_constant_ivm_uptake.R",
@@ -298,6 +323,34 @@ res4 <- runfun(wh4)
 df_4 <- as.data.frame(res4)
 
 
+#NC model with untreated net: dn0 = 0.059, rn0 = 0.409, sn0 = 0.532
+wh6 <- ivRmectin:::create_r_model(odin_model_path = "inst/extdata/odin_endec_mu_h_constant_ivm_uptake.R",
+                                  #num_int = 1,
+                                  num_int = 2,
+                                  itn_cov = 0.6,
+                                  ITN_IRS_on = 100,
+                                  #het_brackets = 5,
+                                  #age = init_age,
+                                  init_EIR = 100,
+                                  #country = "Senegal", # Country setting to be run - see admin_units_seasonal.rds in inst/extdata for more info
+                                  #admin2 = "Fatick",
+                                  ttt = ivm_parms1$ttt,
+                                  eff_len = ivm_parms1$eff_len,
+                                  haz = ivm_parms3$haz,
+                                  ivm_cov_par = ivm_parms1$ivm_cov_par,
+                                  ivm_min_age = ivm_parms1$ivm_min_age,
+                                  ivm_max_age = ivm_parms1$ivm_max_age,
+                                  mu_h = 0.26,
+                                  avhc = avhc_constant,
+                                  IVRM_start = ivm_parms1$IVRM_start,
+                                  d_ITN0 = 0.059,
+                                  r_ITN0 = 0.409)
+
+#run Hannah's model with mu_h of 0.26
+res6 <- runfun(wh6)
+df_6 <- as.data.frame(res6)
+
+
 
 #df_0 : Hannah's model, no nets, ivm
 df_0_epi <- df_0 %>%
@@ -313,6 +366,11 @@ write.csv(df_3_epi, file = "data/llin_ivm_muh/df_3_epi.csv", row.names = FALSE)
 df_3a_epi <- df_3a %>%
   select(t, EIR_tot, slide_prev0to5)
 write.csv(df_3a_epi, file = "data/llin_ivm_muh/df_3a_epi.csv", row.names = FALSE)
+
+#df_5: Hannah's model, untreated nets, ivm
+df_5_epi <- df_5 %>%
+  select(t, EIR_tot, slide_prev0to5)
+write.csv(df_5_epi, file = "data/llin_ivm_muh/df_5_epi.csv", row.names = FALSE)
 
 #df2: Nilani's model, no nets, ivm
 df_2_epi <- df_2 %>%
@@ -330,6 +388,13 @@ df_4_epi <- df_4 %>%
   select(t, EIR_tot, slide_prev0to5)
 write.csv(df_4_epi, file = "data/llin_ivm_muh/df_4_epi.csv", row.names = FALSE)
 
+#df6: NC model, untreated nets, ivm
+
+df_6_epi <- df_6 %>%
+  select(t, EIR_tot, slide_prev0to5)
+write.csv(df_6_epi, file = "data/llin_ivm_muh/df_6_epi.csv", row.names = FALSE)
+
+
 #READ IN THE EPI FILES HERE
 
 #HS models
@@ -344,6 +409,9 @@ df_3_epi <- read.csv("data/llin_ivm_muh/df_3_epi.csv", header = TRUE)
 #HS, NETS only
 df_3a_epi <- read.csv("data/llin_ivm_muh/df_3a_epi.csv", header = TRUE)
 
+#HS, UTN and ivm
+df_5_epi <- read.csv("data/llin_ivm_muh/df_5_epi.csv", header = TRUE)
+
 
 #NC models
 
@@ -357,6 +425,9 @@ df_4_epi <- read.csv("data/llin_ivm_muh/df_4_epi.csv", header = TRUE)
 
 #NC, NETS only
 df_2a_epi <- read.csv("data/llin_ivm_muh/df_2a_epi.csv", header = TRUE)
+
+#NC, IVM + UTN
+df_6_epi <- read.csv("data/llin_ivm_muh/df_6_epi.csv", header = TRUE)
 
 #plots to compare the methods
 NC_HS_model_nets <- ggplot(df_3_epi, aes(x = t, y = EIR_tot))+ #HS
@@ -373,7 +444,7 @@ NC_HS_model_no_nets <-ggplot(df_0_epi, aes(x = t, y = EIR_tot))+ #HS
 
 year_ivm_before_on <- 365*7
 year_period <- 3*365
-y3_end_ivm <-  year_ivm_on + year_period
+y3_end_ivm <-  year_ivm_before_on + year_period
 
 #absolute difference in EIR between df_0 and df_3
 df_0_mean_EIR = df_0_epi %>% #ivm only
@@ -385,8 +456,14 @@ df_3_mean_EIR = df_3_epi %>% #nets + ivm
   filter(between(t, year_ivm_before_on, y3_end_ivm)) %>%
   summarise(mean_EIR = mean(EIR_tot))  # 31.58684. nets + ivm
 
+df_5_mean_EIR = df_5_epi %>% #UTN + ivm
+  filter(between(t, year_ivm_before_on, y3_end_ivm)) %>%
+  summarise(mean_EIR = mean(EIR_tot))  # 105.2722
+
+
 df_0_mean_EIR - df_3_mean_EIR #37.82529
 
+df_0_mean_EIR - df_5_mean_EIR # -35.86002
 
 df_2_mean_EIR = df_2_epi %>% #ivm only
   filter(between(t, year_ivm_before_on, y3_end_ivm)) %>%
@@ -398,24 +475,40 @@ df_4_mean_EIR = df_4_epi %>% # nets + ivm
 
 df_2_mean_EIR - df_4_mean_EIR # 37.6168
 
+df_6_mean_EIR <- df_6_epi %>% #untreated nets and ivm
+  filter(between(t, year_ivm_before_on, y3_end_ivm)) %>%
+  summarise(mean_EIR = mean(EIR_tot)) # 104.5639
+
+df_2_mean_EIR - df_6_mean_EIR #  -35.54429
+
+
 #rel diff HS
 rel_EIR_HS <- ((df_0_mean_EIR-df_3_mean_EIR)/df_0_mean_EIR)*100 #54.49378
+
+#rel diff HS UTN
+rel_EIR_UTN_HS <- ((df_0_mean_EIR-df_5_mean_EIR)/df_5_mean_EIR)*100 #-34.06411
 
 #rel diff NC
 rel_EIR_NC <- ((df_2_mean_EIR-df_4_mean_EIR)/df_2_mean_EIR)*100 # 54.50164
 
+#rel diff NC untreated nets
+rel_EIR_UTN_NC <- ((df_2_mean_EIR - df_6_mean_EIR)/df_2_mean_EIR)*100 #-51.49886
 
 HS_plots_EIR <- ggplot(df_0_epi, aes(x = t, y = EIR_tot))+
   geom_line()+
   geom_line(data = df_3_epi, aes(x = t, y = EIR_tot), col = "red")+
   geom_line(data = df_3a_epi, aes(x = t, y = EIR_tot), col = "blue")+
-  ggtitle("EIR w HS model, ivm only (black), LLIN + IVM (red) and nets only (blue)")
+  geom_line(data = df_5_epi, aes(x = t, y = EIR_tot), col = "purple")+
+  ggtitle("EIR w HS model, ivm only (black), LLIN + IVM (red) and nets only (blue),
+          untreated nets (purple)")
 
 NC_plots_EIR <- ggplot(df_2_epi, aes(x = t, y = EIR_tot))+
   geom_line()+
   geom_line(data = df_4_epi, aes(x = t, y = EIR_tot), col = "red")+
   geom_line(data = df_2a_epi, aes(x = t, y = EIR_tot), col = "blue")+
-  ggtitle("EIR w NC model, iivm only (black), LLIN + IVM (red) and nets only (blue)")
+  geom_line(data = df_6_epi, aes(x = t, y = EIR_tot), col = "purple")+
+  ggtitle("EIR w NC model, ivm only (black), LLIN + IVM (red) and nets only (blue),
+          untreated nets (purple)")
 
 require(cowplot)
 plot_grid(HS_plots_EIR, NC_plots_EIR)
@@ -577,3 +670,245 @@ NC_out_df <- do.call(rbind,
 #out_df_long <- gather(out_df, parameter, value, itn_cov:ref, factor_key = TRUE)
 
 write.csv(NC_out_df, file = "data/llin_ivm_muh/NC_itn_cov_loop.csv", row.names = FALSE)
+
+
+#does the change in mu exceed the change in avhc, which is why we don't see a difference between explicit model of refeeding delay vs not
+#PLOT 2
+#plot of avhc over time - this is the normalised value and what clearly affects the ivm uptake
+#av_mosq and avhc under different LLIN coverages
+out.df <- read.csv("data/out_df.csv", header = TRUE) #made in llin_ivm.R
+
+avhc_plot <- ggplot(out.df, aes(x = t, y = avhc, col = as.factor(itn_cov)))+
+  geom_line(linewidth = 1)+
+  ylim(0, 1)
+
+
+mu_plot <- ggplot(out.df, aes(x = t, y = mu, col = as.factor(itn_cov)))+
+  geom_line(linewidth = 1)+
+  ylim(0, 1)
+
+#between 0 ITN cov and 0.8 ITN cov, what is absolute and rel diff in mu and avhc across full 10y
+
+avhc_summary <- out.df %>%
+  select(t, avhc, mu, itn_cov) %>%
+  group_by(itn_cov) %>%
+  summarise(mean_avhc = mean(avhc),
+            mean_mu = mean(mu)) %>%
+  mutate(abs_diff_avhc  = 0.307 - mean_avhc,
+         rel_diff_avhc = (0.307 - mean_avhc)/0.307,
+         abs_diff_mu = 0.132-mean_mu,
+         rel_diff_mu = (0.132-mean_mu)/0.132)
+
+abs_diff_params <- ggplot(avhc_summary, aes(x = itn_cov, y = abs_diff_avhc, col = as.factor(itn_cov)))+
+  geom_point(shape = 2, size = 4)+
+  geom_point(data = avhc_summary, aes(x = itn_cov, y = abs_diff_mu, fill = as.factor(itn_cov)), shape = 4, size = 4)+
+  ylab("Absolute difference (triangle = avhc, cross = mu) \n compared to net cov 0")+
+  theme_minimal()+
+  ylim(-1.2, 1)
+
+rel_diff_params <- ggplot(avhc_summary, aes(x = itn_cov, y = rel_diff_avhc, col = as.factor(itn_cov)))+
+  geom_point(shape = 2, size = 4)+
+  geom_point(data = avhc_summary, aes(x = itn_cov, y = rel_diff_mu, fill = as.factor(itn_cov)), shape = 4, size = 4)+
+  ylab("Relative difference (triangle = avhc, cross = mu) \n compared to net cov 0")+
+  theme_minimal()+
+  ylim(-1.2, 1)
+
+plot_grid(abs_diff_params, rel_diff_params)
+
+out_df_UTN <- read.csv("data/llin_ivm_muh/out_df_UTN.csv", header = TRUE)
+
+avhc_summary_UTN <- out_df_UTN %>%
+  select(t, avhc, mu, itn_cov) %>%
+  group_by(itn_cov) %>%
+  summarise(mean_avhc = mean(avhc),
+            mean_mu = mean(mu)) %>%
+  mutate(abs_diff_avhc  = 0.307 - mean_avhc,
+         rel_diff_avhc = (0.307 - mean_avhc)/0.307,
+         abs_diff_mu = 0.132-mean_mu,
+         rel_diff_mu = (0.132-mean_mu)/0.132)
+
+abs_diff_params_UTN <- ggplot(avhc_summary_UTN, aes(x = itn_cov, y = abs_diff_avhc, col = as.factor(itn_cov)))+
+  geom_point(shape = 2, size = 4)+
+  geom_point(data = avhc_summary_UTN, aes(x = itn_cov, y = abs_diff_mu, fill = as.factor(itn_cov)), shape = 4, size = 4)+
+  ylab("Absolute difference (triangle = avhc, cross = mu) \n with UTN compared to net cov 0")+
+  theme_minimal()+
+  ylim(-1.2, 1)
+
+rel_diff_params_UTN <- ggplot(avhc_summary_UTN, aes(x = itn_cov, y = rel_diff_avhc, col = as.factor(itn_cov)))+
+  geom_point(shape = 2, size = 4)+
+  geom_point(data = avhc_summary_UTN, aes(x = itn_cov, y = rel_diff_mu, fill = as.factor(itn_cov)), shape = 4, size = 4)+
+  ylab("Relative difference (triangle = avhc, cross = mu) \n with UTN compared to net cov 0")+
+  theme_minimal()+
+  ylim(-1.2, 1)
+
+plot_grid(abs_diff_params, abs_diff_params_UTN, rel_diff_params,  rel_diff_params_UTN,
+          ncol = 2, nrow =2, labels = c("LLIN", "UTN"))
+
+
+
+#exploring different net types
+path_nets <- "C:/Users/nc1115/OneDrive - Imperial College London/PhD/PhD_malaria/data/ellie_net_efficacy"
+IG2_nets <- file.path(path_nets, "pyrethroid_pyrrole_nets.csv")
+pyr_only <- file.path(path_nets, "pyrethroid_only_nets.csv")
+pbo_nets <- file.path(path_nets, "pyrethroid_pbo_nets.csv")
+
+IG2_nets_df <- read.csv(IG2_nets, header = TRUE)
+pyr_only_df <- read.csv(pyr_only, header = TRUE)
+pbo_nets_df <- read.csv(pbo_nets, header = TRUE)
+
+IG2_d_ITN0 <- IG2_nets_df$dn0_med
+
+pyr_d_ITN0 <- pyr_only_df$dn0_med
+
+pbo_d_ITN0 <- pbo_nets_df$dn0_med
+
+#modify the dfs so that the gamma for pbos and pyrrole are same as pyr only
+IG2_nets_df <- IG2_nets_df %>%
+  mutate(gamman_lo10 = pyr_only_df$gamman_lo10,
+         gamman_med = pyr_only_df$gamman_med,
+         gamman_up90 = pyr_only_df$gamman_up90)
+
+
+pbo_nets_df <- pbo_nets_df %>%
+  mutate(gamman_lo10 = pyr_only_df$gamman_lo10,
+         gamman_med = pyr_only_df$gamman_med,
+         gamman_up90 = pyr_only_df$gamman_up90)
+
+#ivm_cov_vector <- seq(0, 1, 0.25)
+
+#make an empty list
+IG2_param_list <- list()
+pyr_param_list <- list()
+pbo_param_list <- list()
+
+#use expand grid to get all combinations of the parameter values
+#param_df <- expand.grid(d_ITN0 = d_ITN0_vector,
+#                        r_ITN0 = r_ITN0_vector) #,
+#ivm_cov = ivm_cov_vector)
+
+itn_cov_vector <- seq(0, 0.8, 0.2)
+
+#only use expand grid on the ivm_cov and dn0, because rn0 is correlated with dn0 so we add it in via left_join
+IG2_param_df_crit <- expand.grid(dn0_med = IG2_d_ITN0, itn_cov = itn_cov_vector) #606 rows
+IG2_param_df <- left_join(IG2_param_df_crit,
+                          IG2_nets_df %>% dplyr::select(dn0_med, rn0_med, resistance, gamman_med),
+                          by = c("dn0_med")) %>%
+  mutate(net_type = "IG2") %>%
+  rename(d_ITN0 = dn0_med, r_ITN0 = rn0_med, itn_half_life = gamman_med)
+
+#repeat for pyrethroid nets
+pyr_param_df_crit <- expand.grid(dn0_med = pyr_d_ITN0, itn_cov = itn_cov_vector)
+pyr_param_df <- left_join(pyr_param_df_crit,
+                          pyr_only_df %>% dplyr::select(dn0_med, rn0_med, resistance, gamman_med),
+                          by = c("dn0_med")) %>%
+  mutate(net_type = "pyrethroid only") %>%
+  rename(d_ITN0 = dn0_med, r_ITN0 = rn0_med, itn_half_life = gamman_med)
+
+#repeat for pbo nets
+pbo_param_df_crit <- expand.grid(dn0_med = pbo_d_ITN0, itn_cov = itn_cov_vector)
+pbo_param_df <- left_join(pbo_param_df_crit,
+                          pbo_nets_df %>% dplyr::select(dn0_med, rn0_med, resistance, gamman_med),
+                          by = c("dn0_med")) %>%
+  mutate(net_type = "pyrethroid pbo") %>%
+  rename(d_ITN0 = dn0_med, r_ITN0 = rn0_med, itn_half_life = gamman_med)
+
+#select a few resistance values: "resistance" 0%, 20%, 60% and 80%
+IG2_param_df_filt <- IG2_param_df %>%
+  filter(resistance %in% c(0, 0.5))
+
+pyr_param_df_filt <- pyr_param_df %>%
+  filter(resistance %in% c(0, 0.5))
+
+pbo_param_df_filt <- pbo_param_df %>%
+  filter(resistance %in% c(0, 0.5))
+
+#conversions to numeric for each df
+for(i in seq_len(nrow(IG2_param_df_filt))){
+  IG2_param_list[[i]] <- as.numeric(IG2_param_df_filt[i,]) #convert to numeric so it's in the right form for runfun
+}
+
+for(i in seq_len(nrow(pyr_param_df_filt))){
+  pyr_param_list[[i]] <- as.numeric(pyr_param_df_filt[i,]) #convert to numeric so it's in the right form for runfun
+}
+
+for(i in seq_len(nrow(pbo_param_df_filt))){
+  pbo_param_list[[i]] <- as.numeric(pbo_param_df_filt[i,]) #convert to numeric so it's in the right form for runfun
+}
+
+
+#function for looping through net coverages and net types
+
+create_itn_type_vec <- function(itn_type_ivm_param){
+  d_ITN0_in <- itn_type_ivm_param[1]
+  itn_cov_in <-itn_type_ivm_param[3]
+  r_ITN0_in <- itn_type_ivm_param[2]
+  itn_half_life <- itn_type_ivm_param[5]
+  output <- ivRmectin::create_r_model(
+    odin_model_path = "inst/extdata/odin_model_endectocide.R",
+    #num_int = 1,
+    num_int = 2, # number of vector control (IRS and ITN) population groups
+    ITN_IRS_on = itn_on,
+    itn_cov = itn_cov_in,
+    #het_brackets = 5, # number of heterogeneous biting categories
+    #age = init_age, # the different age classes to be ran within the model
+    init_EIR = init_EIR, # the Entomological Innoculation Rate
+    #country = "Senegal", # Country setting to be run - see admin_units_seasonal.rds in inst/extdata for more info
+    #admin2 = "Fatick", # Admin 2 setting to be run - see admin_units_seasonal.rds in inst/extdata for more info
+    ttt = ivm_parms1$ttt, # model specific parameter to control timing of endectocide delivery
+    eff_len = ivm_parms1$eff_len, # number of days after receiving endectocide that HR is higher
+    haz = ivm_parms1$haz, # hazard ratio for each off the eff_len number of days
+    ivm_cov_par = 0, # proportion of popuulation receiving the endectocide
+    ivm_min_age = ivm_parms1$ivm_min_age, # youngest age group receiving endectocide
+    ivm_max_age = ivm_parms1$ivm_max_age, # oldest age group receiving endectocide
+    IVRM_start = ivm_parms1$IVRM_start ,
+    d_ITN0 = d_ITN0_in,
+    r_ITN0 = r_ITN0_in,
+    itn_half_life = itn_half_line_in
+  )
+  return(output)
+}
+
+
+
+IG2_out_list <- lapply(IG2_param_list, create_itn_type_vec) #putting param list into the function to generate parameter set
+pyr_out_list <- lapply(pyr_param_list, create_itn_type_vec)
+pbo_out_list <- lapply(pbo_param_list, create_itn_type_vec)
+
+#up to here
+
+
+#run it
+IG2_res_out_4_list <- lapply(IG2_out_4_list, runfun)
+pyr_res_out_4_list <- lapply(pyr_out_4_list, runfun)
+pbo_res_out_4_list <- lapply(pbo_out_4_list, runfun)
+
+
+IG2_out_df_4<- do.call(rbind,
+                       sapply(1:(length(d_ITN0_vector)), function(x){
+                         as.data.frame(IG2_res_out_4_list[[x]]) %>%
+                           select(t, mv, Q, avhc, av_mosq_sum, itn_cov, ivm_cov, Sxtot, Extot, Ixtot, mvxtot, FOIv, lag_FOIv,
+                                  d_ITN0, d_ITN, r_ITN0, r_ITN, s_ITN, ITN_decay, itn_loss, Sx_dead, Ex_dead, Ix_dead, mvx_dead, resistance, net_type) %>%
+                           mutate(ref = x)
+                       }, simplify = F))
+
+write.csv(IG2_out_df_4, file = "data/out_df_4_IG2.csv", row.names = FALSE)
+
+pyr_out_df_4 <- do.call(rbind,
+                        sapply(1:(length(d_ITN0_vector)), function(x){
+                          as.data.frame(pyr_res_out_4_list[[x]]) %>%
+                            select(t, mv, Q, avhc, av_mosq_sum, itn_cov, ivm_cov, Sxtot, Extot, Ixtot, mvxtot, FOIv, lag_FOIv,
+                                   d_ITN0, d_ITN, r_ITN0, r_ITN, s_ITN, ITN_decay, itn_loss, Sx_dead, Ex_dead, Ix_dead, mvx_dead, resistance, net_type) %>%
+                            mutate(ref = x)
+                        }, simplify = F))
+
+write.csv(pyr_out_df_4, file = "data/out_df_4_pyr.csv", row.names = FALSE)
+
+pbo_out_df_4 <- do.call(rbind,
+                        sapply(1:(length(d_ITN0_vector)), function(x){
+                          as.data.frame(pbo_res_out_4_list[[x]]) %>%
+                            select(t, mv, Q, avhc, av_mosq_sum, itn_cov, ivm_cov, Sxtot, Extot, Ixtot, mvxtot, FOIv, lag_FOIv,
+                                   d_ITN0, d_ITN, r_ITN0, r_ITN, s_ITN, ITN_decay, itn_loss, Sx_dead, Ex_dead, Ix_dead, mvx_dead, resistance, net_type) %>%
+                            mutate(ref = x)
+                        }, simplify = F))
+
+write.csv(pbo_out_df_4, file = "data/out_df_4_pbo.csv", row.names = FALSE)
