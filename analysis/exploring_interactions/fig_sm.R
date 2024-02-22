e8373e016a2340e6ac71d7fc649fdfbc9052f02f
nilani-chandradeva1 <nilani.chandradeva15@imperial.ac.uk>
2024-02-22 09:27:51 +0000

    new file for the heatmap

---
+++ analysis/exploring_interactions/fig_sm.R
@@ -0,0 +1,8 @@
+#supp figure to look at
+
+bb_antag_itn_ivm <- readRDS("X:/endectocides-cluster/data/antag_LLIN_IVM_bites_Bed_loop.rds")
+bb_antag_itn <- readRDS("X:/endectocides-cluster/data/antag_LLIN_bites_Bed_loop.rds")
+
+bb_add_itn_ivm <- readRDS("X:/endectocides-cluster/data/add_LLIN_IVM_bites_Bed_loop.rds")
+bb_add_itn <- readRDS("X:/endectocides-cluster/data/add_LLIN_bites_Bed_loop.rds")
+
