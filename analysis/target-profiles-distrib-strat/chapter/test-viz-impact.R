0a55922692281dc15c7b13ac5d97fbffc3aa1399
nilani-chandradeva1 <nilani.chandradeva15@imperial.ac.uk>
2025-07-28 16:40:58 +0100

    all-in-one vs staggered distr plots (perennial)

--- analysis/target-profiles-distrib-strat/chapter/test-viz-impact.R
+++ analysis/target-profiles-distrib-strat/chapter/test-viz-impact.R
@@ -12,15 +12,16 @@ df_distr_all <- readRDS("analysis/target-profiles-distrib-strat/chapter/output/d
 model_types <-  unique(df_distr_all$model_type)
 
 df_distr_all %>%
-  filter(t == 1) %>% #EIR at t = 1 either 1.39 or 69.5
+  filter(t == 1) %>% #EIR at t = 1 0.00407 or 0.203
   group_by(ref) %>%
   summarise(eir = EIRout) %>%
-  distinct()  #so ref 1 is low EIR and 2 is high
+  distinct()  #so ref 1-3 is low EIR and 4-6 is high
 
 #match to the initial EIR passed into the model
 df_distr_all <- df_distr_all %>%
-  mutate(init_EIR = case_when(ref == 1 ~ 2,
-                              TRUE ~ 100))
+  mutate(init_EIR = case_when(ref <= 3 ~ 2,
+                              TRUE ~ 100)) %>%
+  filter(model_type != "all-in-one")
 
 unique(df_distr_all$init_EIR)
 unique(df_distr_all$model_type)
@@ -36,13 +37,11 @@ distr_pals <- c('#66c2a5','#fc8d62','#8da0cb','#e78ac3','#a6d854')
 
 #filter to just high EIR and gamb-like vector (Q0 = 0.92) for plots
 
-covs <- unique(df_distr_all$ivm_cov)
+covs <- unique(df_distr_all$ivm_cov_par)
 
 df_distr <- df_distr_all %>%
   filter(init_EIR == 100)
 
-df_time_covs <- df_distr %>%
-  select(t, ivm_cov, model_type)
 
 #read in the HRs for the staggered
 stag_HR <- readRDS("analysis/target-profiles-distrib-strat/chapter/output/test_HR_staggered_long.rds")
@@ -54,7 +53,7 @@ ivm_haz <- ivm_haz %>%
   select(-IVM_400_1_HS) %>%
   filter(between(Day, 1, 23))
 
-HR_plot <- ggplot(stag_HR, aes(x = Day-1, y = HR, group = group))+
+HR_plot <- ggplot(stag_HR, aes(x = Day, y = HR, group = group))+
   geom_line(aes(col = as.factor(group), lty = as.factor(group_lab)),size = 1.1,
             inherit.aes = TRUE, alpha = 0.6)+
   geom_point(size = 1.5, alpha = 0.8, aes(col = as.factor(group)))+
@@ -78,7 +77,7 @@ HR_plot <- ggplot(stag_HR, aes(x = Day-1, y = HR, group = group))+
   xlim(-10, 107)
 
 
-time_toxic_plot <- ggplot(stag_cov, aes(x = Day-1, y = prop_pop_cov))+
+time_toxic_plot <- ggplot(stag_cov, aes(x = Day, y = prop_pop_cov))+
   geom_line(size = 1.1)+
   facet_wrap(vars(stagger), labeller = label_both)+
   ylab("Proportion of covered group \n with HR > 1")+
@@ -91,24 +90,39 @@ time_toxic_plot <- ggplot(stag_cov, aes(x = Day-1, y = prop_pop_cov))+
 #cowplot::plot_grid(HR_plot, time_toxic_plot, nrow = 2)
 
 df_time_covs <- df_distr %>%
-  select(t, ivm_cov, model_type) %>%
-  filter(model_type %in% c("10d-stagger", "20d-stagger", "all-in-one-stag"))
+  #select(t, ivm_cov, model_type) %>%
+  filter(model_type %in% c("10d-stagger", "20d-stagger", "all-in-one-stag") & ivm_cov_par == 0.7)
 
-time_var_cov_plot <- ggplot(df_time_covs, aes(x = t-start+1, y = ivm_cov*100))+
+time_var_cov_plot <- ggplot(df_time_covs, aes(x = t-start, y = ivm_cov*100))+
   geom_line(size = 1.1)+
   facet_wrap(vars(model_type))+
   xlim(-10, 107)+
   xlab("Time (days) since intervention started")+
   theme_bw()+
-  ylab("Proportion of population with lethal dose of ivermectin (%)")+
+  ylab("Proportion of population with \n lethal dose of ivermectin (%)")+
   ylim(0,60)
 
 cowplot::plot_grid(HR_plot, time_toxic_plot, time_var_cov_plot,
                    nrow = 3, align = "v",
                    labels = c("A", "B", "C"))
 
+checking_covs <- df_time_covs %>%
+  filter(model_type == "all-in-one-stag")
 
-distr_pals <- c('#e41a1c','#377eb8','#4daf4a','#984ea3')#measurements taken at diff time points for trials
+
+
+
+checking_covs$ivm_cov[start:2055] == checking_covs$ivm_cov[2055:2085]
+
+ivm_parms_all$IVRM_start[start:2055] #goes to 5475 at pos 24
+ivm_parms_all$IVRM_start[2021:2055]
+checking_covs$ivm_cov[start:2055] #goes to 0 at pos 23
+checking_covs$ivm_cov[2021:2025] #the start seems to come on one day earlier. acually on day 2024 instead of 2025. Think need to change the ivm_mda function
+ivm_parms_all_stag$prop_human_HR_threshold[2021:2025]
+ivm_parms_all_stag$IVRM_start[2021:2025]
+
+
+distr_pals <- c('#66c2a5','#fc8d62','#8da0cb','#a6d854')
 #MATAMAL: 4 weeks after last MDA (prevalence)
 #BOHEMIA: incidence from first MDA, for 6 months
 matamal_survey <- (30*4) + start
@@ -117,23 +131,32 @@ plot_matamal <- matamal_survey-start #diff between start and survey
 bohemia_inc_period <- start+(6*30)
 plot_bohemia <- bohemia_inc_period- start
 
-#by 0.28y since int, the mosquito density is back to equilibrium
+second_mda <- 30+start
+third_mda <- 60+start
+
+
+df_distr <- df_distr %>%
+  filter(ivm_cov_par == 0.7)
+
+#dotted arrow shows point of matamal prevalence survey
+#bound box is bohemia incidence period
+#full shaded area is area that I measure
 mv_plot <- ggplot(df_distr, aes(x = (t - start)/365, y = mv, col = as.factor(model_type))) +
   geom_line(size = 1.1) +
   theme_bw() +
   ylim(0, 50) +
   ylab("Mosquito density")+
-  theme(legend.position = c(0.6, 0.9),
-        legend.direction = "horizontal") +
+  theme(legend.position = c(0.5, 0.1)) +
 
-  scale_color_manual(name = "Scenario", values = distr_pals) +
+  scale_color_manual(values = distr_pals, labels = c("10 days", "20 days", "1 day"),
+                     name = "Time to complete MDA") +
   xlim(-0.25, 1) +
   xlab("Years since intervention started") +
   geom_segment(aes(x = 0, y = 45, xend = 0, yend = 42), arrow = arrow(),
                col = "black", size = 1.1)+
   geom_segment(aes(x = plot_matamal/365, y = 45, xend = plot_matamal/365, yend = 42),
                arrow = arrow(),
-               col = "black", size = 1.1, lty = "dotted")+
+               col = "blue", size = 1.1)+
   annotate("rect", xmin = 0, xmax = plot_bohemia/365, ymin = 20, ymax = 42,
            fill = "white", alpha = 0.1, col = "black")+
   annotate("rect", xmin = 0, xmax = 1, fill = "blue", ymin = 0, ymax = 42,alpha = 0.1)
@@ -214,7 +237,7 @@ dynamics <- cowplot::plot_grid(mv_plot, eir_plot, inc_plot, prev_plot, align = "
 
 
 df_distr_wide_setting_ivm <- df_distr_all %>%
-  select(t,ref, init_EIR, model_type, clin_inc0to5) %>%
+  select(t,ref, init_EIR, model_type, ivm_cov_par, clin_inc0to5) %>%
   group_by(ref) %>%
   group_modify(~ {
     df_wide <- .x %>%
@@ -235,10 +258,12 @@ df_distr_wide_setting_baseline <- df_distr_all %>%
 df_distr_wide_setting <- left_join(df_distr_wide_setting_ivm,df_distr_wide_setting_baseline, by = c("ref",
                                                                                                     "t",
 
+
                                                                                                     "init_EIR"))
 unique(df_distr_all$model_type)
+
 df_distr_wide_setting_prev_ivm<- df_distr_all %>%
-  select(t, ref, init_EIR, model_type, slide_prev0to5) %>%
+  select(t, ref, init_EIR, model_type, ivm_cov_par, slide_prev0to5) %>%
   group_by(ref) %>%
   group_modify(~ {
     df_wide <- .x %>%
@@ -267,7 +292,7 @@ df_distr_wide_setting_prev <- left_join(df_distr_wide_setting_prev_ivm,df_distr_
 #up to here
 
 impact_overall <- df_distr_wide_setting %>%
-  group_by(ref, init_EIR) %>%
+  group_by(ref, init_EIR, ivm_cov_par) %>%
   #group_by(ref) %>%
   filter(between(t, start, start + 365)) %>% #sum cases one year from start
   summarise(tot_cases_baseline = sum(baseline)*1000,
@@ -284,7 +309,7 @@ impact_overall <- df_distr_wide_setting %>%
 
 
 impact_bohemia <- df_distr_wide_setting %>%
-  group_by(ref, init_EIR) %>%
+  group_by(ref, init_EIR, ivm_cov_par) %>%
   filter(between(t, start, bohemia_inc_period)) %>%
   summarise(tot_cases_baseline = sum(baseline)*1000,
             tot_cases_10d = sum(`10d-stagger`)*1000,
@@ -298,7 +323,7 @@ impact_bohemia <- df_distr_wide_setting %>%
          scenario = "bohemia") #measuring impact across 6 months
 
 impact_matamal <- df_distr_wide_setting_prev %>%
-  group_by(ref, init_EIR) %>%
+  group_by(ref, ivm_cov_par, init_EIR) %>%
   filter(t == matamal_survey) %>%
   summarise(prev_baseline = baseline,
             prev_10d =`10d-stagger`,
@@ -313,7 +338,7 @@ impact_matamal <- df_distr_wide_setting_prev %>%
 
 
 impact_measurements <- do.call("rbind", list(impact_overall, impact_bohemia, impact_matamal)) %>%
-  select(init_EIR, impact_all_in, impact_all_in_stag, impact_20d, impact_10d, scenario)
+  select(init_EIR, ivm_cov_par, impact_all_in, impact_all_in_stag, impact_20d, impact_10d, scenario)
 
 impact_measurements_long <- impact_measurements %>%
   pivot_longer(cols = c(impact_all_in, impact_all_in_stag, impact_20d, impact_10d), names_to = "intervention", values_to = "impact")
@@ -323,26 +348,52 @@ levels_x <- unique(impact_measurements_long$scenario)
 distr_pals <- c('#66c2a5','#fc8d62','#8da0cb','#e78ac3','#a6d854')
 distr_pals2 <- distr_pals[1:4]
 
-impact_main_plot <- impact_measurements_long %>%
-  filter(init_EIR == 100) %>%
-  ggplot()+
-  aes(x = factor(scenario), y = impact, fill = as.factor(intervention))+
-  geom_bar(stat = "identity", position = position_dodge())+
-  theme_bw()+
-  theme(legend.position = c(0.7, 0.8))+
-  scale_fill_manual(values = distr_pals2, name = "Scenario",
-                    labels = c("10 days to complete monthly MDA",
-                               "20 days to complete monthly MDA",
-                               "1 day to complete monthyl MDA (original model)",
-                               "1 day to complete monthly MDA (staggered model)"))+
-  xlab("Time of measurement")+
-  ylab("Difference(%) in endpoint compared to baseline")+
-  #ylim(0, 60)+
-  #guides(fill = "none")+
-  #facet_wrap(vars(ivm_cov))+
-  scale_x_discrete(labels = c("bohemia" = "Incidence U5s (start to 6m later)",
-                              "matamal" = "Prevalence U5s (1m after last MDA)",
-                              "Once year since start" = "Incidence U5s (start to 1y later)"))
+cov_error <- impact_measurements_long %>%
+  ungroup() %>%
+  select(-ref) %>%
+  filter(init_EIR == 100 & ivm_cov_par %in% c(0.5, 0.9)) %>%
+  pivot_wider(names_from = ivm_cov_par, values_from = impact) %>%
+  rename(cov_low_0.5 = `0.5`,
+         cov_high_0.9 = `0.9`)
+
+impact_main_plot <- ggplot() +
+  # Bars
+  geom_bar(
+    data = impact_measurements_long %>% filter(init_EIR == 100 & ivm_cov_par == 0.7),
+    aes(x = factor(scenario), y = impact, fill = as.factor(intervention)),
+    stat = "identity",
+    position = position_dodge(width = 0.9)
+  ) +
+  # Error bars
+  geom_errorbar(
+    data = cov_error,
+    aes(
+      x = factor(scenario),
+      ymin = cov_low_0.5,
+      ymax = cov_high_0.9,
+      fill = as.factor(intervention)   # match fill to align dodging
+    ),
+    width = 0.2,
+    position = position_dodge(width = 0.9)
+  ) +
+  theme_bw() +
+  theme(legend.position = c(0.7, 0.8)) +
+  scale_fill_manual(
+    values = distr_pals2, name = "Scenario",
+    labels = c(
+      "10 days to complete monthly MDA",
+      "20 days to complete monthly MDA",
+      "1 day to complete monthyl MDA (original model)",
+      "1 day to complete monthly MDA (staggered model)"
+    )
+  ) +
+  xlab("Time of measurement") +
+  ylab("Difference(%) in endpoint compared to baseline") +
+  scale_x_discrete(labels = c(
+    "bohemia" = "Incidence U5s (start to 6m later)",
+    "matamal" = "Prevalence U5s (1m after last MDA)",
+    "Once year since start" = "Incidence U5s (start to 1y later)"
+  ))
 
 
 dynamics_perennial <- cowplot::plot_grid(mv_plot, eir_plot, prev_plot, inc_plot,
